2cba03d751947a08632c93d8130e2b0592a4c411
nguyenanhtu123 <91925390+nguyenanhtu1001@users.noreply.github.com>
2023-09-23 17:54:11 +0700

    Delete shipment-service-develop/shipment-service-develop directory

--- shipment-service-develop/shipment-service-develop/shipment-server/src/main/java/com/ncsgroup/shipment/server/configuration/ServiceConfiguration.java
+++
@@ -1,7 +0,0 @@
-package com.ncsgroup.shipment.server.configuration;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ServiceConfiguration {
-}
--- shipment-service-develop/shipment-service-develop/shipment-server/src/main/java/com/ncsgroup/shipment/server/exception/base/BadRequestException.java
+++
@@ -1,9 +0,0 @@
-package com.ncsgroup.shipment.server.exception.base;
-
-
-public class BadRequestException extends BaseException {
-  public BadRequestException() {
-    setCode("com.ncsgroup.profiling.exception.base.BadRequestException");
-    setStatus(StatusConstants.BAD_REQUEST);
-  }
-}
