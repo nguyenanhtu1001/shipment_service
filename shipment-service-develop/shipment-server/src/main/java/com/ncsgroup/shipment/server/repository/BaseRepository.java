a7b2a5e1abd22f035112510155aeff8519460960
nguyenanhtu123 <91925390+nguyenanhtu1001@users.noreply.github.com>
2023-09-23 18:06:58 +0700

    Delete shipment-service-develop directory

--- shipment-service-develop/shipment-server/src/main/java/com/ncsgroup/shipment/server/repository/BaseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ncsgroup.shipment.server.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.NoRepositoryBean;
-
-@NoRepositoryBean
-public interface BaseRepository<T> extends JpaRepository<T, String> {
-}
